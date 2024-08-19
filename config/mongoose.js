702d6d1dc55aafa28d6dac7f7b1e635a037fed3f
Al Helal Abdullah <alhelal@Als-MacBook-Air.local>
2024-08-19 08:54:04 +0600

    tenant portal ok

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect(process.env.MONGODB_URI);
 
-// mongoose.connect(process.env.MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const connection = mongoose.connection;
 module.exports = connection;
