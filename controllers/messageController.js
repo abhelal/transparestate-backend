a2d6705b4958568af2c49e0a4a9eaefb7c543b33
Al Helal Abdullah <alhelal@Als-MacBook-Air.local>
2024-10-12 22:54:07 +0600

    added archive

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -4,9 +4,38 @@ const Message = require("../models/messageModel");
 const Maintenance = require("../models/maintenanceModel");
 const User = require("../models/userModel");
 
+exports.getArchivedConversations = async (req, res) => {
+  let query = {
+    client: req.client,
+    archivedBy: { $eq: req.id },
+  };
+
+  if (req.role === USER_ROLES.TENANT) {
+    query.tenant = req.id;
+  }
+
+  if (req.role === USER_ROLES.MANAGER || req.role === USER_ROLES.MAINTAINER || req.role === USER_ROLES.JANITOR) {
+    const staff = await User.findById(req.id);
+    const properties = staff.properties;
+    query.property = { $in: properties };
+  }
+
+  const conversations = await Conversation.find(query)
+    .populate("property", "name")
+    .populate("maintenance", "maintenanceType maintenanceDetails")
+    .populate({
+      path: "messages",
+      options: { sort: { createdAt: -1 } },
+      perDocumentLimit: 1,
+    })
+    .sort("-updatedAt");
+  res.status(200).json({ conversations });
+};
+
 exports.getConversations = async (req, res) => {
   let query = {
     client: req.client,
+    archivedBy: { $ne: req.id },
   };
 
   if (req.role === USER_ROLES.TENANT) {
@@ -101,4 +130,13 @@ exports.startConversation = async (req, res) => {
     return res.status(200).json({ conversationId: newConversation.conversationId });
   }
   res.status(200).json({ conversationId: conversation.conversationId });
+};
+
+exports.archiveConversation = async (req, res) => {
+  const { conversationId } = req.params;
+  const conversation = await Conversation.findOne({ conversationId });
+  conversation.archived = true;
+  conversation.archivedBy.push(req.id);
+  await conversation.save();
+  res.status(200).json({ message: "Conversation archived" });
 };
--- routes/v1/messageRoutes.js
+++ routes/v1/messageRoutes.js
@@ -7,10 +7,16 @@ const { USER_PERMISSIONS } = require("../../constants");
 
 // :: Prefix Path ---  '/api/v1/messages'
 router.get("/", permissionCheck(USER_PERMISSIONS.READ_MESSAGE), catchErrors(messageController.getConversations));
+router.get("/archived", permissionCheck(USER_PERMISSIONS.READ_MESSAGE), catchErrors(messageController.getArchivedConversations));
 router.get("/recent-messages", permissionCheck(USER_PERMISSIONS.READ_MESSAGE), catchErrors(messageController.getRecentMessages));
 router.get("/:conversationId", permissionCheck(USER_PERMISSIONS.READ_MESSAGE), catchErrors(messageController.getMessages));
 
 router.post("/:conversationId", permissionCheck(USER_PERMISSIONS.UPDATE_MESSAGE), catchErrors(messageController.sendMessage));
+router.put(
+  "/:conversationId/archive",
+  permissionCheck(USER_PERMISSIONS.UPDATE_MESSAGE),
+  catchErrors(messageController.archiveConversation)
+);
 router.post("/:maintenanceId/start", permissionCheck(USER_PERMISSIONS.UPDATE_MESSAGE), catchErrors(messageController.startConversation));
 
 module.exports = router;
